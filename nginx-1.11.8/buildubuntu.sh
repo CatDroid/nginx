07953035e99bc2b7e48dd17bf8edcd1d208022a7
hanlon <1198432354@qq.com>
2017-01-24 15:14:40 +0800

    更正编译脚本 添加使用说明

--- nginx-1.11.8/buildubuntu.sh
+++ nginx-1.11.8/buildubuntu.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-INSTALL_DIR=./myubuntu
-RTMP_MODULE=../nginx-rtmp-module
-SBIN_PATH=${INSTALL_DIR}/sbin/nginx
-MODULE_PATH=${INSTALL_DIR}/modules/
-CONF_PATH=${INSTALL_DIR}/conf/nginx.conf
+INSTALL_DIR=$(pwd)/myubuntu
 
-LOG_DIR=${INSTALL_DIR}/logs/
-PID_PATH=${LOG_DIR}/nginx.pid
-LOCK_PATH=${LOG_DIR}/nginx.lock
-ERROR_LOG_PATH=${LOG_DIR}/error.log
+#只要指定了--prefix 所有的 nginx.conf nginx logs都会安装在prefex下面
+#SBIN_PATH=${INSTALL_DIR}/sbin/nginx
+#MODULE_PATH=${INSTALL_DIR}/modules/
+#CONF_PATH=${INSTALL_DIR}/conf/nginx.conf
+
+#LOG_DIR=${INSTALL_DIR}/logs/
+#PID_PATH=${LOG_DIR}/nginx.pid
+#LOCK_PATH=${LOG_DIR}/nginx.lock
+#ERROR_LOG_PATH=${LOG_DIR}/error.log
+
+# with-openssl with-zlib 等是依赖库的源码位置 不是库位置  
+# objs/Makefile 可以查看到他们会编译成 .a 静态库  --disable-shared
+
+./configure --prefix=${INSTALL_DIR} \
+		--with-openssl=../openssl-1.0.2j \
+		--with-pcre=../pcre-8.40 \
+		--with-zlib=../zlib-1.2.11 \
+		--add-module=../nginx-rtmp-module
 
 
-./configure --prefix=./myubuntu \
-	
-	--add-module=../nginx-rtmp-module
+
+#		--sbin-path=${SBIN_PATH} \
+#		--modules-path=${MODULE_PATH} \
+#		--conf-path=${CONF_PATH} \
+#		--error-log-path=${ERROR_LOG_PATH} \
+#		--pid-path=${PID_PATH} \
+#		--lock-path=${LOCK_PATH} \
